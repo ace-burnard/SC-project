ab07ef3ee0d04cd050690f8cc39ac433cc757920
ace-burnard <126224455+ace-burnard@users.noreply.github.com>
2023-06-01 16:15:50 +0930

    Update 2023-03-19_mixed-measures-ANOVA.R

--- R/2023-03-19_mixed-measures-ANOVA.R
+++ R/2023-03-19_mixed-measures-ANOVA.R
@@ -1,66 +1,66 @@
 pacman::p_load(tidyverse, readr, rstatix, ggpubr)
 data <- read_csv(here::here("data/2023-03-01_gene-data.csv"))
 
-# ##example
-# library(datarium)
-# data("performance", package = "datarium")
-# performance <- performance %>%
-#   gather(key = 'time', value = 'score', t1, t2) %>%
-#   convert_as_factor(id, time)
-# bxp <- ggboxplot(performance,
-#                  x = 'gender',
-#                  y = 'score',
-#                  color = 'stress',
-#                  palette = 'jco',
-#                  facet.by = 'time')
-# bxp
-# #check assumptions
-# performance %>%
-#   group_by(gender, stress, time) %>%
-#   identify_outliers(score)
-# normality_test <- performance %>%
-#   group_by(gender, stress, time) %>%
-#   shapiro_test(score)
-# normality_test %>% arrange(p)
-# ggqqplot(performance, 'score', ggtheme = theme_bw()) +
-#   facet_grid(time ~ stress, labeller = "label_both")
-# homogeneity_test <- performance %>%
-#   group_by(time) %>%
-#   levene_test(score ~ gender*stress)
-# homogeneity_test %>% arrange(p)
-# #three-way mixed ANOVA
-# res.aov <- anova_test(data = performance,
-#                       dv = score,
-#                       wid = id,
-#                       within = time,
-#                       between = c(gender, stress))
-# get_anova_table(res.aov)
-# #post-hoc
-# # two-way interaction at each time levels
-# two.way <- performance %>%
-#   group_by(time) %>%
-#   anova_test(dv = score, wid = id, between = c(gender, stress))
-# two.way
-# # simple simple main effect
-# stress.effect <- performance %>%
-#   group_by(time, gender) %>%
-#   anova_test(dv = score, wid = id, between = stress)
-# stress.effect# %>% filter(time == "t2")
-# # simple simple pairwise comparisons
-# pwc <- performance %>%
-#   group_by(time, gender) %>%
-#   pairwise_t_test(score ~ stress, p.adjust.method = "bonferroni") %>%
-#   select(-p, -p.signif) # Remove details
-# pwc %>% filter(time == "t2", gender == "female")
-# # Visualization: box plots with p-values
-# pwc <- pwc %>% add_xy_position(x = "gender")
-# pwc.filtered <- pwc %>% filter(time == "t2", gender == "female")
-# bxp +
-#   stat_pvalue_manual(pwc.filtered, tip.length = 0, hide.ns = TRUE) +
-#   labs(
-#     subtitle = get_test_label(res.aov, detailed = TRUE),
-#     caption = get_pwc_label(pwc)
-#   )
+##example
+library(datarium)
+data("performance", package = "datarium")
+performance <- performance %>%
+  gather(key = 'time', value = 'score', t1, t2) %>%
+  convert_as_factor(id, time)
+bxp <- ggboxplot(performance,
+                 x = 'gender',
+                 y = 'score',
+                 color = 'stress',
+                 palette = 'jco',
+                 facet.by = 'time')
+bxp
+#check assumptions
+performance %>%
+  group_by(gender, stress, time) %>%
+  identify_outliers(score)
+normality_test <- performance %>%
+  group_by(gender, stress, time) %>%
+  shapiro_test(score)
+normality_test %>% arrange(p)
+ggqqplot(performance, 'score', ggtheme = theme_bw()) +
+  facet_grid(time ~ stress, labeller = "label_both")
+homogeneity_test <- performance %>%
+  group_by(time) %>%
+  levene_test(score ~ gender*stress)
+homogeneity_test %>% arrange(p)
+#three-way mixed ANOVA
+res.aov <- anova_test(data = performance,
+                      dv = score,
+                      wid = id,
+                      within = time,
+                      between = c(gender, stress))
+get_anova_table(res.aov)
+#post-hoc
+# two-way interaction at each time levels
+two.way <- performance %>%
+  group_by(time) %>%
+  anova_test(dv = score, wid = id, between = c(gender, stress))
+two.way
+# simple simple main effect
+stress.effect <- performance %>%
+  group_by(time, gender) %>%
+  anova_test(dv = score, wid = id, between = stress)
+stress.effect# %>% filter(time == "t2")
+# simple simple pairwise comparisons
+pwc <- performance %>%
+  group_by(time, gender) %>%
+  pairwise_t_test(score ~ stress, p.adjust.method = "bonferroni") %>%
+  select(-p, -p.signif) # Remove details
+pwc %>% filter(time == "t2", gender == "female")
+# Visualization: box plots with p-values
+pwc <- pwc %>% add_xy_position(x = "gender")
+pwc.filtered <- pwc %>% filter(time == "t2", gender == "female")
+bxp +
+  stat_pvalue_manual(pwc.filtered, tip.length = 0, hide.ns = TRUE) +
+  labs(
+    subtitle = get_test_label(res.aov, detailed = TRUE),
+    caption = get_pwc_label(pwc)
+  )
 
 #actual data
 data_aov <- data %>%
